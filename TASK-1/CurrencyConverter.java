fb699d09d029f322fca19dc5e7ce20a0d4891bd6
lalli1509 <141894093+lalli1509@users.noreply.github.com>
2023-09-14 22:21:36 +0530

    CurrencyConverter.java

--- TASK-1/CurrencyConverter.java
+++ TASK-1/CurrencyConverter.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//package Currency_Converter;
-
+//WE NEED TO IMPORT PACKAGES FOR CURRENCY CONVERSION
 
 public class CurrencyConverter extends javax.swing.JFrame {
 double US_Dollar = 1.31;
